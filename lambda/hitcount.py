d5c622157d39f764087860440fa5b0efa8fe20b6
EC2 Default User <ec2-user@ip-10-10-0-5.ap-southeast-1.compute.internal>
2021-05-21 02:50:45 +0000

    HitCounter Complete

--- lambda/hitcount.py
+++ lambda/hitcount.py
@@ -12,13 +12,13 @@ def handler(event, context):
     table.update_item(
         Key={'path': event['path']},
         UpdateExpression='ADD hits :incr',
-        ExpressionAttributesValues={':incr': 1}
+        ExpressionAttributeValues={':incr': 1}
         )
     resp = _lambda.invoke(
         FunctionName=os.environ['DOWNSTREAM_FUNCTION_NAME'],
-        PayLoad=json.dumps(event),
+        Payload=json.dumps(event),
         )
-    body = resp['PayLoad'].read()
+    body = resp['Payload'].read()
     
     print('downstream response: {}'.format(body))
     return json.loads(body)
